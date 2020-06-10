39a1efa6a3a9cc9e879135738ee17d8a4628642e
just-a-cat <deathdrake+githubjac@gmail.com>
2020-06-10 07:42:38 +0200

    Hm

---
+++ lesson-4-5/js/main.js
@@ -0,0 +1,20 @@
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amount = parseInt(amountInput.value) * price;
+    let showAmount = document.querySelector("span.show-amount");
+
+    let amountNumber = parseInt(amountInput.value);
+
+    amountNumber = isNaN(amountNumber) ? 0 : amountInput;
+    
+    if (amountNumber > 10) {
+        alert("You cannot order more than 10 Meowrgers!");
+    }
+    else if (amountNumber < 1) {
+        alert("You have to buy at least 1 Meowrger");
+    } else {
+        showAmount.innerHTML = amount;
+    }
+
+}
